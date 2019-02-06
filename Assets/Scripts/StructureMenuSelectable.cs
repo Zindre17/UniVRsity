2e9e0c2b23b0ac44a8313c0893bc8b8832eec47c
Zindre17 <zindre17@gmail.com>
2019-02-06 15:19:15 +0100

    noe

---
+++ Assets/Scripts/StructureMenuSelectable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructureMenuSelectable : MenuSelectable
+{
+
+    new enum MenuOption { }
+    
+}
